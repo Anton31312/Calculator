0c49ab645dd7e67c7f3cb14ad95758494be430bb
Anton31312 <fearthe54@gmail.com>
2021-06-21 23:29:11 +0300

    Add files via upload

---
+++ Calculator/Function.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Function
+    {
+        private double a = 0;
+
+        public void Put_A(double a)
+        {
+            this.a = a;
+        }
+
+        public void Clear_A()
+        {
+            a = 0;
+        }
+
+        public double Multiplication(double b)
+        {
+            return a * b;
+        }
+
+        public double Division(double b)
+        {
+            return a / b;
+        }
+
+        public double Sum(double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double b) 
+        {
+            return a - b;
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Function C;
+        public const double PI = 3.141592653589793;
+        public const double E = 2.7182818284;
+
+     
+
+        private void CorrectNumber()
+        {
+            //Если есть знак "бесконечность" - не даёт писать цифры после него
+            if (lblNumber.Text.IndexOf("∞") != -1)
+                lblNumber.Text = lblNumber.Text.Substring(0, lblNumber.Text.Length - 1);
+
+            //Округление
+            if (lblNumber.Text[0] == '0' && (lblNumber.Text.IndexOf(",") != 1))
+                lblNumber.Text = lblNumber.Text.Remove(0, 1);
+
+            //Округление для отрицательного числа
+            if (lblNumber.Text[0] == '-')
+                if (lblNumber.Text[1] == '0' && (lblNumber.Text.IndexOf(",") != 2))
+                    lblNumber.Text = lblNumber.Text.Remove(1, 1);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            C = new Function();
+            lblNumber.Text = "0";
+        }
+
+
+        private void btnE_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += Convert.ToString(E);
+            CorrectNumber();
+        }
+
+        private void btnClear_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text = "0";
+
+            C.Clear_A();
+        }
+
+        private void btnNumTochka_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((lblNumber.Text.IndexOf(",") == -1) && (lblNumber.Text.IndexOf("∞") == -1))
+            {
+                lblNumber.Text += ".";
+            }
+                
+        }
+
+        private void btnPi_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += Convert.ToString(PI);
+            CorrectNumber();
+        }
+
+        private void btnNum7_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "7";
+            CorrectNumber();
+        }
+
+        private void btnNum8_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "8";
+            CorrectNumber();
+        }
+
+        private void btnNum9_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "9";
+            CorrectNumber();
+        }
+
+        private void btnNum4_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "4";
+            CorrectNumber();
+        }
+
+        private void btnNum5_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "5";
+            CorrectNumber();
+        }
+
+        private void btnNum6_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "6";
+            CorrectNumber();
+        }
+
+        private void btnNum1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "1";
+            CorrectNumber();
+        }
+
+        private void btnNum2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "2";
+            CorrectNumber();
+        }
+
+        private void btnNum3_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "3";
+            CorrectNumber();
+        }
+
+        private void btnNum0_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "0";
+            CorrectNumber();
+        }
+
+        private void btnNumRavno_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           
+                lblNumber.Text = C.Multiplication(Convert.ToDouble(lblNumber.Text)).ToString();
+
+                lblNumber.Text = C.Division(Convert.ToDouble(lblNumber.Text)).ToString();
+
+                lblNumber.Text = C.Sum(Convert.ToDouble(lblNumber.Text)).ToString();
+
+                lblNumber.Text = C.Subtraction(Convert.ToDouble(lblNumber.Text)).ToString();
+
+        }
+
+        private void btnNumSkobka1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "(";
+        }
+
+        private void btnNumSkobka2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += ")";
+        }
+
+        private void btnSin_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnDeg_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnNumDelit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            C.Put_A(Convert.ToDouble(lblNumber.Text));
+
+            lblNumber.Text += "/";
+        }
+
+        private void btnNumMulti_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            C.Put_A(Convert.ToDouble(lblNumber.Text));
+
+            lblNumber.Text += "х";
+        }
+
+        private void btnNumMinus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lblNumber.Text += "-";
+            C.Put_A(Convert.ToDouble(lblNumber.Text));
+        }
+
+        private void btnNumPlus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            C.Put_A(Convert.ToDouble(lblNumber.Text));
+
+            lblNumber.Text += "+";
+        }
+    }
+}
